ef84719a7a1fa1dcd8d05e711f9bd3be6c6f137b
Syedhamza245 <155845626+Syedhamza245@users.noreply.github.com>
2024-08-20 15:55:18 -0700

    Update config-overrides.js

--- myapp/config-overrides.js
+++ myapp/config-overrides.js
@@ -1,12 +1,10 @@
-// const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
-  // ... your existing configuration
+  
   Object.assign(fallback, {
     zlib: require.resolve("browserify-zlib"),
-    
     querystring: require.resolve("querystring-es3"),
     path: require.resolve("path-browserify"),
     crypto: require.resolve("crypto-browserify"),
@@ -16,20 +14,17 @@ module.exports = function override(config) {
     url: require.resolve("url/"),
     buffer: require.resolve("buffer/"),
     util: require.resolve("util/"),
-    "assert": require.resolve("assert/"),
+    assert: require.resolve("assert/"),
     net: false, // net is not available in the browser, so set it to false
+  });
 
-  })
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      process: "process/browser"
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"], // Added for buffer support
     })
-  ])
+  ]);
 
   return config;
-     
-
-  // You may also need to include other necessary configurations depending on your project
 };
-
