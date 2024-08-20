9ca79acf0744797bc120106347f5d3124c80057a
Syedhamza245 <155845626+Syedhamza245@users.noreply.github.com>
2024-08-20 15:43:36 -0700

    Update Signin.jsx

--- myapp/src/components/Signin.jsx
+++ myapp/src/components/Signin.jsx
@@ -11,7 +11,7 @@ const Signin = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://fyp-backend1-qz2b.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
